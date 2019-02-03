2c770a2d5262e39c08e1ac0094f3a2e1f49ceceb
Tobias Borgen <tobias.borgen@protonmail.com>
2019-02-03 17:34:44 +0100

    This is just getting silly

--- ctftime/__init__.py
+++ ctftime/__init__.py
@@ -4,6 +4,9 @@ import os
 try:
     os.mkdir('data')
 
+except:
+    pass
+
 logging.basicConfig(filename='./data/log.txt', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
 logger = logging.getLogger(__name__)
